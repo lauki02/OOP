6d3b96d54e6b8ec5732d7f2e8b944bdd18a51143
corta <corta@DESKTOP-VDSMCKK.fritz.box>
2023-02-16 12:44:07 +0100

    CSVReader

---
+++ OOP/src/RotWeissGmbH/CSV_Reader_New.java
@@ -0,0 +1,59 @@
+package RotWeissGmbH;
+
+
+import java.io.*;
+import java.util.ArrayList;
+
+import javax.swing.JList; 
+
+public class CSV_Reader_New {
+
+	 ArrayList<Serviceauftrag>auftraege= new ArrayList<Serviceauftrag>();
+	 JList list_1 = new JList();
+	 ArrayList <Mitarbeiter> mitarbeiterListe = new ArrayList <Mitarbeiter> ();
+	 JList list_2 = new JList();
+	 ArrayList <Kunde> kundenListe= new ArrayList <Kunde>();
+	 JList list_3= new JList ();
+	
+	
+	
+	
+	
+
+	 
+	 public void readCSVdata(ArrayList<Serviceauftrag>auftraege) {
+	 
+	 
+		 try {
+			 FileReader reader = new FileReader ("");
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+		 		} catch(IOException e) {
+					e.printStackTrace();
+					}
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	 
+	 
+	 
+	 
+	 
+	 }
+	 
+}
